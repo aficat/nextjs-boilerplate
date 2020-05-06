720a529f185f1f2c8ffbade27f9c7ed3005b134a
aficat <aficatrashid@gmail.com>
2020-05-06 22:47:11 +0800

    updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
-    exportPathMap: function () {
-        return {
-            "/": { page: "/" },
-            "/about": { page: "/about" },
-        }
-    },
-    assetPrefix: !debug ? 'https://aficat.github.io/nextjs-boilerplate/' : '',
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' }
+    }
+  },
+  assetPrefix: !debug ? '/nextjs-boilerplate/' : '',
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
+  }
 }
