ba4a9e33256ee5874e5b4d875048ac3d50fb3bca
aficat <aficatrashid@gmail.com>
2020-05-06 22:55:12 +0800

    updated links

--- components/Header.js
+++ components/Header.js
@@ -7,10 +7,10 @@ const linkStyle = {
 
 const Header = () => (
     <div>
-        <Link href="/" as={ process.env.BACKEND_URL + '/'}>
+        <Link href="/">
           <a style={linkStyle}>Home</a>
         </Link>
-        <Link href="/about" as={ process.env.BACKEND_URL + '/about'}>
+        <Link href="/about">
           <a style={linkStyle}>About</a>
         </Link>
     </div>
